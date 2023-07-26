ca8b47870da402b171c0dd94f332f57051dd132c
Disa11 <stevenknauth3@gmail.com>
2023-07-26 01:02:32 -0600

    Ocultar los elementos mientras se carga la pagina

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 var preloader = document.getElementById("preloader");
+const element = document.getElementsByClassName("tagvisibility");
 
 window.addEventListener("load", () => {
   preloader.style.display = "none";
-  document.getElementById('body_id').classList.remove('hidden')
+  document.getElementById('body_id').classList.remove('hidden');
+
+  for (let i = 0; i < element.length; i++) {
+    element[i].style.visibility = "visible"
+  }
 })
